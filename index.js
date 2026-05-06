0bcf9f45d4862a0bbd4a03662629261736873c89
Ishimwe Alphonsine <ishimwealphonsine01@gmail.com>
2026-05-06 09:06:04 +0300

    new stockRoutes, addstock.pug files

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const app = express();
 const port = 3000;
 
 // 3. Configuration
+// Set templating engine to pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
 
 // 4. Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
 // Logging middleware to log the time of each request
 app.use((req, res, next) => {
   console.log('A new request received at ' + Date.now());
   next();
 });
-
-app.use(express.urlencoded({ extended: true }));
 
 app.use(expressSession({
   secret: 'thisIsMyNyondoStockProject',
@@ -24,19 +28,12 @@ app.use(expressSession({
   saveUninitialized: false
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 // 5. Routes
-
-// Serving html files
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-
-
-
+app.use('/', require('./routes/indexRoutes'));
+app.use('/', require('./routes/stockRoutes'));
 
 // Handling non-existent routes
 app.use((req, res) => {
