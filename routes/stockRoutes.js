0eec17901e1b9c67a4a5815d05ee9cca47e497f0
Ishimwe Alphonsine <ishimwealphonsine01@gmail.com>
2026-05-07 10:55:17 +0300

    new html files

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// stock dashboard
+router.get('/stock-management', (req, res) => {
+  res.render('stockmanagement');
+});
+router.post('/stockmanagement', (req, res) => {
+  console.log(req.body);
+});
+
 router.get('/add-stock', (req, res) => {
   res.render('addstock');
 });
 router.post('/add-stock', (req, res) => {
   console.log(req.body);
 });
+
+
 
 
 
